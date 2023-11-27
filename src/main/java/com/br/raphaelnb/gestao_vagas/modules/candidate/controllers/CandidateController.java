6815c1eea75a13af269ef1ec9987bdcf128effa0
Raphael Nobre <nobre.roots@gmail.com>
2023-11-27 08:59:41 -0300

    Adicionando Interface Candidate e JPA REpository

--- src/main/java/com/br/raphaelnb/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/com/br/raphaelnb/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -1,11 +1,13 @@
 package com.br.raphaelnb.gestao_vagas.modules.candidate.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.br.raphaelnb.gestao_vagas.modules.candidate.CandidateEntity;
+import com.br.raphaelnb.gestao_vagas.modules.candidate.CandidateRepository;
 
 import jakarta.validation.Valid;
 
@@ -13,9 +15,13 @@ import jakarta.validation.Valid;
 @RequestMapping("/candidate")
 public class CandidateController {
 
+    @Autowired
+    private CandidateRepository candidateRepository;
+
     @PostMapping("/")
-    public void create(@Valid @RequestBody CandidateEntity candidate) {
+    public CandidateEntity create(@Valid @RequestBody CandidateEntity candidate) {
         System.out.println("email do candidato: " + candidate.getEmail());
+        return candidateRepository.save(candidate);
     }
 
 }
